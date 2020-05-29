bfa17dc7a433be34df942d227e1338a8c907f6c3
walanpat <39136284+walanpat@users.noreply.github.com>
2020-05-28 22:18:02 -0400

    Updates

---
+++ precache-manifest.114ca166852f19b9c97648e4c5393ef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84fa251ea5297e44d87f1541135983f1",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "10b0042c78e156351368",
+    "url": "/react-gh-pages/static/css/main.5e364e59.chunk.css"
+  },
+  {
+    "revision": "331e54b1dac2a148dea5",
+    "url": "/react-gh-pages/static/js/2.75deb62b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.75deb62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b0042c78e156351368",
+    "url": "/react-gh-pages/static/js/main.6182ad96.chunk.js"
+  },
+  {
+    "revision": "89e5cca80662282a3b52",
+    "url": "/react-gh-pages/static/js/runtime-main.752fd247.js"
+  }
+]);
